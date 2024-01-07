d5f1d46ac3ebc8678d8de4fc87f45d834708e8cc
Steven <v4@outlook.com>
2024-01-07 10:47:41 -0600

    Completed View Model

--- RomeApp/ViewModels/LocationsViewModel.swift
+++ RomeApp/ViewModels/LocationsViewModel.swift
@@ -7,14 +7,39 @@
 
 import Foundation
 import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
     
+    
+    //All loaded locations
     @Published var locations: [Location]
+    
+    //Current location on map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    //Current Region
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    // Span
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init () {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first! // will not fail because there is always atleast one item in the array -
+        self.updateMapRegion(location: locations.first!)
     }
     
+    private func updateMapRegion(location: Location) {
+        withAnimation() {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan)
+        }
+    }
 }
